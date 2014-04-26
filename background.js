f5d84af29dc4d7ab0befda36783e3479b84897e4
Jaden Geller <jaden.geller@gmail.com>
2014-04-26 15:06:07 -0700

    buggyyyy

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+console.log("BLAH");
+
+var tabHistories = {};
+
 function addTab(id) {
     tabHistories[id] = new Array;
 }
@@ -7,27 +11,65 @@ function deleteTab(id) {
 }
 
 function addHistoryToTab(history, tab){
-    tabHistories[tab].push(history);
+    var hist = tabHistories[tab];
+    if (hist == null){
+      addTab(tab);
+      hist = tabHistories[tab];
+    }
+    hist.push(history);
+}
+
+function getImportantPart(URL) {
+    return URL.match("http(s)?://[^/]*")[0];
+}
+
+function goBack(tab){
+  chrome.tabs.executeScript(tab.id,{
+    code: 'goBack()'
+  });
 }
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.getCurrent(function (tab) {
-    if (check === "YES") chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
-  });
+chrome.tabs.getSelected(null, function(tab){
 
+      var history = tabHistories[tab.id];
+
+      var indexOfLastDifferentSite = -1;
+      var i = history.length - 2; // indexed at 0 AND don't include current site
+      var current = history[history.length-1];
+      while (indexOfLastDifferentSite === -1){
+          if (getImportantPart(history[i]) !== getImportantPart(current)){
+              indexOfLastDifferentSite = i;
+          }
+          else i--;
+      }
+
+      if (indexOfLastDifferentSite != -1){
+        var numTimesToGoBack = history.length - indexOfLastDifferentSite;
+        for (var i = 0; i < numTimesToGoBack; i++){
+          goBack(tab);
+          history.pop();
+        }
+        console.log(numTimesToGoBack);
+
+      }
     });
 });
 
 chrome.tabs.onCreated.addListener(function(tab){
-  addTab(tab.id);
+    addTab(tab.id);
 });
 
-chrome.tabs.onRemoved.addListener(function(integer tabId, object removeInfo){
-    deleteTab(tab.id);
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
+    deleteTab(tabId);
 });
 
-chrome.tabs.onUpdated.addListener(function(integer tabId, object changeInfo, Tab tab){
-    addHistoryToTab(tab.id);
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+});
+
+chrome.webNavigation.onCommitted.addListener(function(details){
+  if (details["transitionQualifiers"].length === 0 && details["transitionType"] !== "auto_subframe" && details["transitionType"] !== "manual_subframe"){
+    addHistoryToTab(details["url"], details["tabId"]);
+  }
 });
